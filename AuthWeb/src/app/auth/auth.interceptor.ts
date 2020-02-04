7055fa8f9003302eef76ae33c4cfb83c5f336649
Jonathas Tassi <jonathas.tassi@datapar.com.br>
2020-02-04 15:51:46 -0300

    Redirecionar para o login quando o token expirar

--- AuthWeb/src/app/auth/auth.interceptor.ts
+++ AuthWeb/src/app/auth/auth.interceptor.ts
@@ -1,10 +1,18 @@
+import { AuthService } from './services/auth.service';
 import { Injectable } from '@angular/core';
 import {
-    HttpEvent, HttpInterceptor, HttpHandler, HttpRequest
+    HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HttpErrorResponse
 } from '@angular/common/http';
+import { catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
+
+    constructor(
+        private authService: AuthService
+    ) { }
+
     intercept(req: HttpRequest<any>, next: HttpHandler) {
         const token = localStorage.getItem('token:authApp');
         if (token) {
@@ -13,7 +21,19 @@ export class AuthInterceptor implements HttpInterceptor {
                     Authorization: `Bearer ${token}`
                 }
             });
-            return next.handle(authReq);
+            return next.handle(authReq)
+                .pipe(
+                    catchError(
+                        (err) => {
+                            if (err instanceof HttpErrorResponse) {
+                                if (err.status === 401 || err.status === 0) {
+                                    this.authService.logout();
+                                }
+                            }
+                            return throwError(err);
+                        }
+                    )
+                )
         }
         return next.handle(req);
     }
